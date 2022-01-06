be4d690451b368298c516ea0dcda9c8258d47420
Eugene Kopilov <64531188+Evgenk2020@users.noreply.github.com>
2022-01-06 21:02:43 +0200

    Add files via upload

---
+++ revise/src/test_original.h
@@ -0,0 +1,12 @@
+#ifndef TEST_ORIGINAL_H
+#define TEST_ORIGINAL_H
+
+#include <iostream>
+#include <rapidjson/document.h>
+#include "wrfile.h"
+#include "url_version.h"
+#include "read_file.h"
+
+void test_with_original(std::string in_state);
+
+#endif // TEST_ORIGINAL_H
